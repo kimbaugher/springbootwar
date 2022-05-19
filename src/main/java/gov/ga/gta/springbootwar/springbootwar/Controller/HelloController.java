c483e4557bb15bc39a998d22bd0ae3752468c95e
kbaugher <kim.baugher@gta.ga.gov>
2022-05-19 20:38:15 +0100

    Fixed pom.xml

--- src/main/java/gov/ga/gta/springbootwar/springbootwar/Controller/HelloController.java
+++ src/main/java/gov/ga/gta/springbootwar/springbootwar/Controller/HelloController.java
@@ -1,7 +1,7 @@
 package gov.ga.gta.springbootwar.springbootwar.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
